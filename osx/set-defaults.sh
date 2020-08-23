432521f0b96c359b48cf2f3c9fbe021090f672df
Ziv Levy <levi.ziv@gmail.com>
2020-08-23 12:18:08 +0300

    uncooment mac network name

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -8,9 +8,9 @@
 #
 
 # Set computer name
-COMPUTERNAME="zivl-mbp13"
-HOSTNAME='zivl-mbp13'
-LOCALHOSTNAME='zivl-mbp13'
+COMPUTERNAME="zivl-mbp16"
+HOSTNAME='zivl-mbp16'
+LOCALHOSTNAME='zivl-mbp16'
 
 # Ask for the administrator password upfront
 sudo -v
@@ -23,10 +23,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-#sudo scutil --set ComputerName $COMPUTERNAME
-#sudo scutil --set HostName $HOSTNAME
-#sudo scutil --set LocalHostName $LOCALHOSTNAME
-#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $LOCALHOSTNAME
+sudo scutil --set ComputerName $COMPUTERNAME
+sudo scutil --set HostName $HOSTNAME
+sudo scutil --set LocalHostName $LOCALHOSTNAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $LOCALHOSTNAME
 
 ###############################################################################
 # Apple software: Safari, Updater, iTunes, etc.                               #
